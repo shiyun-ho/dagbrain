e225bf3829ebd86c011261bdfd6b61872412c4a0
ShiYun <serene.hoshiyun@gmail.com>
2025-07-09 16:05:32 +0800

    [feat] Added function which detects if DAG is cyclic

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,5 +1,5 @@
 class ft_Node<T> {
-    static idCounter = 0;
+   static idCounter = 0;
     id: number;
     data: T;
     neighbours: Set<ft_Node<T>>;
@@ -33,17 +33,40 @@ class directedAcyclicGraph<T> {
     }
 
     addEdge(source: ft_Node<T>, target: ft_Node<T>): boolean {
-        // TODO: Check if edge would not result in cycle
-        if (hasCycleDFS(source, ?, ?, ?)){
+        const visited = new Set<number>;
+        const recursionStack = new Set<number>;
+
+        if (this.hasCycleDFS(source, visited, recursionStack)){
             return false;   
         }
-        else {
-            return true;
-        }
+    
+        source.neighbours.add(target);
+        return true;
     }
 
-    // TODO: Implement, using DFS, to detect cycles. This ensures graph remains acylic
     private hasCycleDFS(startNode: ft_Node<T>, visited: Set<number>, recursionStack: Set<number>): boolean {
+        let startNodeId = startNode.id;
+    
+        if (!visited.has(startNodeId)){
+            visited.add(startNodeId);
+            recursionStack.add(startNodeId);
+
+            for (const neighbour of startNode.neighbours) {
+                if (!visited.has(neighbour.id)){
+                    if(this.hasCycleDFS(neighbour, visited, recursionStack)) {
+                        return true;
+                    }
+                }
+                else {
+                    if (recursionStack.has(neighbour.id)){
+                        return true;
+                    }
+                }
+            }
+
+            recursionStack.delete(startNodeId);
+        }
         return false;
     }
+
 }
